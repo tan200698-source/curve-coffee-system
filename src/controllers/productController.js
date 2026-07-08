ab41127d6f75b9d73e8332d194bea9233146c210
TAN <tan@MacBook-Air-khxng-TAN.local>
2026-07-07 17:48:49 -0700

    Add product detail API

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -52,10 +52,66 @@ const getProductVariants = async (req, res, next) => {
     }
 };
 
+const getProductById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const productResult = await pool.query(
+            `
+            SELECT
+            p.id,
+            p.name,
+            p.description,
+            p.image_url,
+            p.is_available,
+            p.is_featured,
+            p.available_from,
+            p.available_until,
+            p.display_order,
+            c.name AS category
+            FROM products p
+            JOIN categories c
+            ON p.category_id = c.id
+            WHERE p.id = $1;
+            `,
+            [id]
+        );
+        if (productResult.rows.length === 0) {
+            return res.status(404).json({
+                message: "Product not found",
+            });
+        }
+        const variantResult = await pool.query(
+            `
+            SELECT
+            id,
+            name,
+            price,
+            is_available,
+            display_order,
+            image_url
+            FROM product_variants
+            WHERE product_id = $1
+            ORDER BY display_order ASC;
+            `,
+            [id]
+        );
+
+        const product = productResult.rows[0];
+
+        product.variants = variantResult.rows;
+
+        res.json(product);
+    } catch (error) {
+        next(error);
+    }
+};
+
 
 
 
 module.exports = {
     getAllProducts,
     getProductVariants,
+    getProductById,
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,11 +3,14 @@ const express = require("express");
 const{
     getAllProducts,
     getProductVariants,
+    getProductById,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
+
+router.get("/:id", getProductById);
 
 router.get("/:id/variants", getProductVariants);
 
