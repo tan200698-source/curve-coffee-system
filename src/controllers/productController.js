51edc69ebef34443d9e2ff328235e43ba6dfc243
TAN <tan@MacBook-Air-khxng-TAN.local>
2026-07-08 18:26:33 -0700

    Add product search and category filter

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,29 +2,58 @@ const pool = require("../db");
 
 const getAllProducts = async (req, res, next) => {
     try {
-        const result = await pool.query(`
-            SELECT
-            p.id,
-            p.name,
-            p.description,
-            p.image_url,
-            p.is_available,
-            p.is_featured,
-            p.available_from,
-            p.available_until,
-            p.display_order,
-            c.name AS category
-            FROM products p
-            JOIN categories c
-            ON p.category_id = c.id
-            ORDER BY p.display_order ASC;
-            `);
+        const { search, category } = req.query;
 
-            res.json(result.rows);
+        let query = `
+        SELECT
+        p.id,
+        p.name,
+        p.description,
+        p.image_url,
+        p.is_available,
+        p.is_featured,
+        p.available_from,
+        p.available_until,
+        p.display_order,
+        c.name AS category
+        FROM products p
+        JOIN categories c
+        ON p.category_id = c.id
+        `;
+
+        const values = [];
+
+        if (search) {
+            values.push(`%${search}%`);
+            query += ` WHERE p.name ILIKE $${values.length}`;
+        }
+
+        if (category) {
+            values.push(category);
+
+            if (search) {
+                query += ` AND c.name = $${values.length}`;
+            } else {
+                query += ` WHERE c.name = $${values.length}`;
+            }
+        }
+
+        query += `
+        ORDER BY p.display_order ASC;
+        `;
+
+        const result = await pool.query(query, values);
+
+        res.json(result.rows);
+
+        
     } catch (error) {
         next(error);
     }
 };
+
+
+
 
 //api ProductVariants
 const getProductVariants = async (req, res, next) => {
