9b24eef1727cb373cfca4b9a373fb7f4bf67eea6
TAN <tan@MacBook-Air-khxng-TAN.local>
2026-07-11 13:13:50 -0700

    Add globla error handling

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (error, req, res, next) => {
+    console.error(error);
+
+    const statusCode = error.statusCode || 500;
+
+    res.status(statusCode). json({
+        message: error.message || "Internal server error",
+    });
+};
+
+module.exports = errorHandler;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const pool = require("./db");
 const productRoutes = require("./routes/productRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const storeRoutes = require("./routes/storeRoutes");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
 
 
 
@@ -15,6 +17,9 @@ app.use(express.json());
 app.use("/products", productRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/store", storeRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
     res.send("curve-coffee-system");
@@ -32,6 +37,11 @@ app.get("/test-db", async (req, res) => {
         });
     }
 });
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
